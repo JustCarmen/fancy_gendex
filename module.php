71babb687b26f0f380268ec4d3b3805545d736a3
JustCarmen <carmen@justcarmen.nl>
2014-08-15 22:23:37 +0200

    Code without side-effects (see PSR-1) does not require guard headers

--- module.php
+++ module.php
@@ -20,11 +20,6 @@
 
 use WT\Auth;
 
-if (!defined('WT_WEBTREES')) {
-	header('HTTP/1.0 403 Forbidden');
-	exit;
-}
-
 class fancy_gendex_WT_Module extends WT_Module implements WT_Module_Config {
 	
 	public function __construct() {
